3ebf594236280df42a09c78928b3486ca446fd77
d4vis <dbukuru1@gmail.com>
2021-04-09 23:46:00 +0300

    Downgrade sqlalchemy for heroku compatibility

--- src/url_app/settings.py
+++ src/url_app/settings.py
@@ -5,7 +5,7 @@ import os
 class Settings(BaseSettings):
     server_host: str = '0.0.0.0'
     server_port: int = os.environ.get('PORT', 8000)
-    database_url: str = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    database_url: str = os.environ.get('DATABASE_URL')
 
 
 settings = Settings(_env_file='.env', _env_file_encoding='utf-8')
